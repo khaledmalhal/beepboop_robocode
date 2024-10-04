99698b260e759559050b92f2e936c7fc1d31d5cb
khaledmalhal <khaled.malhal@estudiantat.upc.edu>
2024-10-04 10:15:24 +0200

    First commit

---
+++ src/beepboop/BeepBoop.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package beepboop;
+import robocode.*;
+
+/**
+ *
+ * @author kmalhal
+ */
+public class BeepBoop extends Robocode{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void run() {
+        
+    }
+    
+}
